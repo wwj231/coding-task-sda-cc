2b37c449b0b2f74a29310032e8501f35f22b92b1
Wladyslaw Wiktor Janicki <wwj231@gmail.com>
2021-11-18 00:01:16 +0100

    Implementation of three methods.

--- src/main/java/com/example/demo/Task.java
+++ src/main/java/com/example/demo/Task.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Task {
 
@@ -9,12 +10,10 @@ public class Task {
    */
 
   public static List<Person> getActivePlayersByScoreDesc(List<Person> people) {
-
-    String x = """
-        This is first line
-        This is second line
-        """;
-    throw new RuntimeException("Not Implemented!");
+    return people.stream()
+            .filter(Person::isActive)
+            .sorted(Comparator.comparing(Person::getScore).reversed())
+            .collect(Collectors.toList());
   }
 
   /*
@@ -22,7 +21,11 @@ public class Task {
    */
 
   public static List<Person> getActivePlayersByScoreDesc(List<Person> people, Group group) {
-    throw new RuntimeException("Not Implemented!");
+    return people.stream()
+            .filter(p -> p.getTeam().equals(group))
+            .filter(Person::isActive)
+            .sorted(Comparator.comparing(Person::getScore).reversed())
+            .collect(Collectors.toList());
   }
 
   /*
@@ -30,7 +33,43 @@ public class Task {
   Jeżeli ta liczba jest również równa, zwróć którąkolwiek z nich.
    */
   public static Group getGroupWithHighestScore(List<Person> people) {
-    throw new RuntimeException("Not Implemented!");
+    HashMap<Group, Integer> groupScoresMap = new HashMap<>();
+    Group groupWithMaxScore = Group.G1;
+    Integer maxScore = 0;
+
+    for (Group group: Group.values()) {
+      Integer score = people.stream().filter(p -> p.getTeam().equals(group)).map(Person::getScore).reduce(0, Integer::sum);
+      if(score.compareTo(maxScore) > 0){
+        groupWithMaxScore = group;
+      }
+      groupScoresMap.put(group, score);
+    }
+
+    if(Collections.frequency(groupScoresMap.values(), maxScore) > 1){
+      List<Group> groupList = groupScoresMap.entrySet().stream().filter(n -> n.getValue().equals(maxScore)).map(Map.Entry::getKey).collect(Collectors.toList());
+
+      long maxScoreGroupMembers = 0L;
+      for (Person person : people) {
+        if (person.getTeam().equals(groupWithMaxScore)) {
+          if (person.isActive()) {
+            maxScoreGroupMembers++;
+          }
+        }
+      }
+
+      for (Group group : groupList) {
+        long groupMembers = people.stream()
+                .filter(p -> p.getTeam().equals(group))
+                .filter(Person::isActive).count();
+
+          if (groupMembers < maxScoreGroupMembers){
+            groupWithMaxScore = group;
+            maxScoreGroupMembers = groupMembers;
+          }
+      }
+      return groupWithMaxScore;
+    }
+    return groupWithMaxScore;
   }
 
   /*
@@ -39,6 +78,7 @@ public class Task {
    */
 
   public static List<String> printPoints(List<Person> people) {
+
     throw new RuntimeException("Not Implemented!");
   }
 }
